caa8f6040f813f2bd68f9adf27184b7cb68673ef
hlnikniky <orekhovs09@gmail.com>
2026-01-26 02:04:41 +0700

    Add change strategy menu

--- src/froms/changestrategyform.py
+++ src/froms/changestrategyform.py
@@ -3,13 +3,15 @@ import npyscreen
 class ChangeStrategyForm(npyscreen.FormBaseNew):
     def create(self):
 
-        self.add(npyscreen.ButtonPress, name="Выбрать в ручную", when_pressed_function=self.back_to_config)
+        self.add(npyscreen.ButtonPress, name="Выбрать в ручную", when_pressed_function=self.manual_selection)
         self.add(npyscreen.ButtonPress, name="Автоматический подбор стратегии", when_pressed_function=self.back_to_config)
+        self.add(npyscreen.FixedText, value="")
         self.add(npyscreen.ButtonPress, name="Назад в настройки", when_pressed_function=self.back_to_config)
 
     
     def manual_selection(self):
-        self.parentApp.switchForm('UNKNOWN')
+        
+        self.parentApp.switchForm('MANUALSTRATEGY')
         
     def auto_selection(self):
         self.parentApp.switchForm('UNKNOWN')
