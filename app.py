6cb01fd4dc212db36d0526a979f857676f684d1b
hlnikniky <orekhovs09@gmail.com>
2026-01-26 02:06:02 +0700

    update app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.froms.configform import ConfigForm
 from src.froms.unknown import Unknown
 from src.froms.serviceform import ServiceForm
 from src.froms.changestrategyform import ChangeStrategyForm
+from src.froms.manualstrategy import ManualChangeStrategy
 
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
@@ -12,6 +13,7 @@ class App(npyscreen.NPSAppManaged):
         self.addForm('SERVICE', ServiceForm, name="Zapret2 Control Panel - Service Management")
         self.addForm('CHANGEIPSET', Unknown, name="Configuration - Change IP Set (Coming Soon)")
         self.addForm('CHANGESTRATEGY', ChangeStrategyForm, name="Configuration - Change Strategy")
+        self.addForm('MANUALSTRATEGY', ManualChangeStrategy, name="Configuration - Manual Strategy Selection")
         
         self.addForm('UNKNOWN', Unknown, name="Zapret2 Control Panel - Coming Soon")
         
